2c89ecd5e0ff33c40ee2a9347aafbf4f2a4a4a6b
Christopher Markieta <markietachristopher@gmail.com>
2021-11-09 09:51:53 -0500

    Adding BC Gov header, footer, styling, and setting default log destination to stdout

--- build/report.py
+++ build/report.py
@@ -43,12 +43,14 @@ def getObjects(type, namespace='default'):
     data = json.loads(oc('get', type, '-n', namespace, '-o', 'json'))
     logging.info("read objects data of type " + type)
   except:
-    logging.error("unable to read object data of type " + type)
+    logging.error("unable to read object data of type " + type + " from namespace " + namespace)
     return []
 
   if "items" in data.keys():
+    logging.info("read " + str(len(data['items'])) + " objects of type " + type + " from namespace " + namespace)
     return data["items"]
 
+  logging.info("didn't find any objects of type " + type + " in namespace " + namespace)
   return []
 #end
 
@@ -105,20 +107,19 @@ def pdbCheck(workloadData):
 
 def writeReport(filename, results, serverName, namespace):
   file = open(filename, 'w')
-  file.write("<html>\n")
-  file.write("<head>\n")
+  file.write("<html>\n<head>\n")
+  file.write('<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap-reboot.min.css" rel="stylesheet">' + "\n")
+  file.write('<link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">' + "\n")
+  file.write('<link rel="stylesheet" href="style.css">' + "\n")
   file.write("<style>\ntable, th, td {\n  border: 1px solid black;\n}\n</style>\n")
-  file.write("<title>report generated on " + datetime.now().strftime("%Y-%m-%d-%H:%M:%S") + "</title>\n")
-  file.write("</head>\n")
-  file.write("<body>\n")
-
-  file.write("<h1>Workload health check report for namespace " + namespace + " on server " + serverName + "\n")
-  file.write("<hr>\n")
-  file.write("<table>\n")
+  file.write("<title>report generated on " + datetime.now().strftime("%Y-%m-%d-%H:%M:%S") + "</title>\n</head>\n<body>\n")
+  file.write("<header>\n" + '<div class="banner">' + "\n" + '<a href="https://gov.bc.ca">' + "\n")
+  file.write('<img src="https://developer.gov.bc.ca/static/BCID_H_rgb_rev-20eebe74aef7d92e02732a18b6aa6bbb.svg" alt="Go to the Government of British Columbia website" height="50px"/>' + "\n")
+  file.write("</a>\n<h1>Hello British Columbia</h1>\n</div>\n" + '<div class="other">' + "\n" + '&nbsp;' + "\n</div>\n</header>\n" + '<p style="padding-top:50px"></p>' + "\n")
+  file.write("<h1>Workload health check report for namespace " + namespace + " on server " + serverName + "\n<hr>\n<table>\n")
 
   workloadNames = results[next(iter(results))].keys()
-  file.write("<tr>\n")
-  file.write("<td>Target</td>")
+  file.write("<tr>\n<td>Target</td>")
   for workloadName in workloadNames:
     file.write("<td>" + workloadName + "</td>")
   file.write("\n</tr>\n")
@@ -131,32 +132,38 @@ def writeReport(filename, results, serverName, namespace):
     file.write("\n</tr>\n")
   #end
 
-  file.write("</table>\n")
-  file.write("<hr>\n")
+  file.write("</table>\n<hr>\n")
 
   for workloadName in workloadNames:
-    file.write("<table>\n")
-    file.write("<tr><td>Target</td><td>" + workloadName + "</td></tr>\n")
+    file.write("<table>\n<tr><td>Target</td><td>" + workloadName + "</td></tr>\n")
 
     for checkName in results.keys():
       file.write("<tr><td>"+checkName+"</td><td style=\"background-color: " + results[checkName][workloadName]['color'] + "\"><pre>" + results[checkName][workloadName]['text'] + "</pre></td></tr>\n")
+    #end
 
-    file.write("</table>\n")
-    file.write("<hr>\n")
+    file.write("</table>\n<hr>\n")
+  #end
 
-  file.write("</body>\n")
-  file.write("</html>\n")
+  file.write('<footer class="footer">' + "\n" + '<div class="container">' + "\n<ul>\n" + '<li><a href=".">Home</a></li>' + "\n" + '<li><a href=".">Disclaimer</a></li>' + "\n")
+  file.write('<li><a href=".">Privacy</a></li>' + "\n" + '<li><a href=".">Accessibility</a></li>' + "\n" + '<li><a href=".">Copyright</a></li>' + "\n")
+  file.write('<li><a href=".">Contact Us</a></li>' + "\n</ul>\n</div>\n</footer>\n</body>\n</html>\n")
   file.close()
 #end
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-l", help="log file name", type=str, default="report.log")
+parser.add_argument("-l", help="log file name", type=str, default="-")
 parser.add_argument("-o", help="output file name", type=str, default="report.html")
 parser.add_argument('-n', help="namespace", type=str, default="default")
 args = parser.parse_args()
 
-logging.basicConfig(filename=args.l, level=logging.INFO, format='[%(asctime)s] %(levelname)s: %(message)s', datefmt='%Y-%m-%d-%H:%M:%S')
+logstream = None
+if args.l == '-':
+  logstream = sys.stdout
+else:
+  logstream = open(args.l, 'a')
+
+logging.basicConfig(stream=logstream, level=logging.INFO, format='[%(asctime)s] %(levelname)s: %(message)s', datefmt='%Y-%m-%d-%H:%M:%S')
 logging.info("generating report")
 
 namespace = args.n
